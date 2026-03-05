97daf83a4f9be0619eb7641ad080a08d16131b12
Manos <rexmanos08@gmail.com>
2026-03-06 01:55:22 +0200

    fixes on shell experiments

--- chordify/experiments/readfile.sh
+++ chordify/experiments/readfile.sh
@@ -4,5 +4,5 @@ input_file="$1"
 
 while read -r x; do
     y=$(tr -dc 'a-zA-Z0-9' </dev/urandom | head -c 8)
-    echo "insert $x $y"
+    echo "insert \"$x\" $y"
 done < "$input_file"
--- chordify/experiments/test.sh
+++ chordify/experiments/test.sh
@@ -1,27 +1,71 @@
 #!/bin/bash
 
-cd target/debug
+cd ../target/debug
 
-gnome-terminal -- bash -c './chordify 127.0.0.1:8000 -k 3 -t 1; exec bash'
+k="$1"
+t="$2"
+
+gnome-terminal -- bash -c './chordify 127.0.0.1:8000 -k "$k" -t "$t"; exec bash'
 
 gnome-terminal -- bash -c ' time (
     sleep 6
-    ../../readfile.sh ../../insert_00_part.txt
+    ../../experiments/readfile.sh ../../experiments/insert/insert_00_part.txt
     echo "depart"
 ) | ./chordify 127.0.0.1:8001 127.0.0.1:8000; exec bash' &
 
 gnome-terminal -- bash -c ' time (
     sleep 6
-    ../../readfile.sh ../../insert_00_part.txt
+    ../../experiments/readfile.sh ../../experiments/insert/insert_01_part.txt
     echo "depart"
 ) | ./chordify 127.0.0.1:8002 127.0.0.1:8000; exec bash' &
 
 gnome-terminal -- bash -c ' time (
     sleep 6
-    ../../readfile.sh ../../insert_00_part.txt
+    ../../experiments/readfile.sh ../../experiments/insert/insert_02_part.txt
     echo "depart"
 ) | ./chordify 127.0.0.1:8003 127.0.0.1:8000; exec bash' &
 
+gnome-terminal -- bash -c ' time (
+    sleep 6
+    ../../experiments/readfile.sh ../../experiments/insert/insert_03_part.txt
+    echo "depart"
+) | ./chordify 127.0.0.1:8004 127.0.0.1:8000; exec bash' &
+
+gnome-terminal -- bash -c ' time (
+    sleep 6
+    ../../experiments/readfile.sh ../../experiments/insert/insert_04_part.txt
+    echo "depart"
+) | ./chordify 127.0.0.1:8005 127.0.0.1:8000; exec bash' &
+
+gnome-terminal -- bash -c ' time (
+    sleep 6
+    ../../experiments/readfile.sh ../../experiments/insert/insert_05_part.txt
+    echo "depart"
+) | ./chordify 127.0.0.1:8006 127.0.0.1:8000; exec bash' &
+
+gnome-terminal -- bash -c ' time (
+    sleep 6
+    ../../experiments/readfile.sh ../../experiments/insert/insert_06_part.txt
+    echo "depart"
+) | ./chordify 127.0.0.1:8007 127.0.0.1:8000; exec bash' &
+
+gnome-terminal -- bash -c ' time (
+    sleep 6
+    ../../experiments/readfile.sh ../../experiments/insert/insert_07_part.txt
+    echo "depart"
+) | ./chordify 127.0.0.1:8008 127.0.0.1:8000; exec bash' &
+
+gnome-terminal -- bash -c ' time (
+    sleep 6
+    ../../experiments/readfile.sh ../../experiments/insert/insert_08_part.txt
+    echo "depart"
+) | ./chordify 127.0.0.1:8009 127.0.0.1:8000; exec bash' &
+
+gnome-terminal -- bash -c ' time (
+    sleep 6
+    ../../experiments/readfile.sh ../../experiments/insert/insert_09_part.txt
+    echo "depart"
+) | ./chordify 127.0.0.1:8010 127.0.0.1:8000; exec bash' &
 
 
 
